9e10c3bdcc06f295c0b4fa02cc3a145b967e5872
Sabastian Auth <sabastian@elderlake.com>
2023-11-09 16:38:16 -0500

    Fix open sign not showing when no artists are available

--- islands/OpenSign.tsx
+++ islands/OpenSign.tsx
@@ -37,37 +37,31 @@ export default function OpenSign() {
     });
   }, []);
 
-  return artists.length > 0
-    ? (
-      <div class="mb-4">
-        <details open={artists.length > 0}>
-          <summary
-            class={`font-sans p-4 font-bold text-3xl ${
-              artists.length > 0
-                ? "bg-green-600 text-white"
-                : "bg-red-600 text-white"
-            }`}
-          >
-            {artists.length > 0
-              ? "Artists available for walk-ins"
-              : "No artists available for walk-ins"}
-          </summary>
-          <ul class="list-disc list-inside border-l border-b border-r border-gray-400 text-xl p-2 pl-6">
-            {artists.map((item) => (
-              <li>
-                <a href="#" class="text-blue-600 underline">{item}</a> -{" "}
-                <span>
-                  Book now (<abbr title="Work-in-progress">WIP</abbr>)
-                </span>
-              </li>
-            ))}
-          </ul>
-        </details>
-      </div>
-    )
-    : (
-      <div class="bg-black font-sans mb-4 p-4 font-bold text-center text-3xl">
-        Retrieving availability
-      </div>
-    );
+  return (
+    <div class="mb-4">
+      <details open={artists.length > 0}>
+        <summary
+          class={`font-sans p-4 font-bold text-3xl ${
+            artists.length > 0
+              ? "bg-green-600 text-white"
+              : "bg-red-600 text-white"
+          }`}
+        >
+          {artists.length > 0
+            ? "Artists available for walk-ins"
+            : "No artists available for walk-ins"}
+        </summary>
+        <ul class="list-disc list-inside border-l border-b border-r border-gray-400 text-xl p-2 pl-6">
+          {artists.map((item) => (
+            <li>
+              <a href="#" class="text-blue-600 underline">{item}</a> -{" "}
+              <span>
+                Book now (<abbr title="Work-in-progress">WIP</abbr>)
+              </span>
+            </li>
+          ))}
+        </ul>
+      </details>
+    </div>
+  );
 }
